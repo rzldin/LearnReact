a02e02fd9426282b5466c0d1eb0038942a2d4bae
rzldin <rizaljihadudin@gmail.com>
2020-09-18 10:43:56 +0700

    first push

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// Class Component Footer
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>ini footer</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+// Class Component Header
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Ini Header</h2>
+            </div>
+        );
+    }
+}
+
+export default Header;
